c96f280af1dcd9c02d08fb466d4abe9e843173fd
royeradames <raadamesbonilla@pplweb.com>
2024-05-26 03:22:55 -0400

    update tailwind: colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,17 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        'purple': '#633CFF',
+        'purple-hover': '#BEADFF',
+        'light-purple': '#EFEBFF',
+        'dark-grey': '#333333',
+        'grey': '#737373',
+        'borders': '#D9D9D9',
+        'light-grey': '#FAFAFA',
+        'white': '#FFFFFF',
+        'red': '#FF3939',
+      },
     },
   },
   plugins: [],
