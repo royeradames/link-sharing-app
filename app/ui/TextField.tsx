643e8a679389d917ab247bbabac84e1d1c16e0d4
royeradames <raadamesbonilla@pplweb.com>
2024-05-27 14:39:20 -0400

    update text field: update styles

--- app/ui/TextField.tsx
+++ app/ui/TextField.tsx
@@ -9,46 +9,46 @@ import { FieldErrors, useForm, UseFormRegister } from "react-hook-form"
 import { zodResolver } from "@hookform/resolvers/zod"
 
 // Define a single schema for the text field
-const TextFieldSchema = z.string().min(1, "Text Field is required")
+export const TextFieldSchema = z.string().min(1, "Text Field is required")
 
-type TextFieldProps = {
+export type TextFieldProps = {
   name: string
   placeholder: string
   register: UseFormRegister<any>
   errors: FieldErrors<any>
 }
 
-const TextField = ({ register, errors, name, placeholder }: TextFieldProps) => {
-  const inputClass = clsx(
-    "flex items-center gap-2 border rounded-lg py-2.5 px-4 text-base font-semibold",
-    {
-      //   "border-gray-300 text-gray-500": control._getWatch() === "empty",
-      //   "border-gray-300 text-black": state === "filled",
-      //   "border-purple-500 text-black outline-none ring-2 ring-purple-500":
-      //     state === "active",
-      "border-red-500 text-red-500": errors[name]?.message,
-    }
-  )
-
+export const TextField = ({
+  register,
+  errors,
+  name,
+  placeholder,
+}: TextFieldProps) => {
   return (
-    <Form.Field className="mb-4" name={name}>
-      <Form.Label className="sr-only">{placeholder}</Form.Label>
-      <Form.Control asChild>
-        <div className={inputClass}>
-          <Link2Icon className="h-5 w-5" />
-          <input
-            className="flex-1 bg-transparent focus:outline-none"
-            placeholder={placeholder}
-            {...register(name)}
-          />
-        </div>
-      </Form.Control>
-      {errors[name]?.message && (
-        <Form.Message className="text-red-500 mt-1 text-sm">
-          {`${errors[name]?.message}`}
-        </Form.Message>
+    <div
+      className={clsx(
+        "flex items-center gap-2 border rounded-lg py-2.5 px-4 text-base font-semibold  focus-within:border-purple-500 focus-within:outline-none focus-within:ring-2 ",
+        {
+          "border-gray-300 text-black focus-within:ring-purple-500  ":
+            !errors[name]?.message,
+          "border-red text-red focus-within:ring-red ": errors[name]?.message,
+        }
       )}
-    </Form.Field>
+    >
+      <Link2Icon className="h-5 w-5 text-grey" />
+
+      <input
+        className={clsx(
+          "flex-1 bg-transparent focus:outline-none font-regular leading-[150%] text-base placeholder:font-regular placeholder:leading-[150%] placeholder:text-base",
+          {
+            "placeholder-gray-500": !errors[name]?.message,
+            "placeholder-red": errors[name]?.message,
+          }
+        )}
+        placeholder={placeholder}
+        {...register(name)}
+      />
+    </div>
   )
 }
 
@@ -67,6 +67,7 @@ const FormDemo = () => {
     },
   })
   const onSubmit = (data: any) => console.log(data)
+  console.log(errors.textField?.message)
   return (
     <Form.Root className="w-[300px] p-4" onSubmit={handleSubmit(onSubmit)}>
       <h2 className="text-xl font-semibold mb-4">Text Field</h2>
@@ -77,7 +78,7 @@ const FormDemo = () => {
         placeholder="Text Field"
       />
       <Form.Submit asChild>
-        <Button type="submit">Post question</Button>
+        <Button>Post question</Button>
       </Form.Submit>
     </Form.Root>
   )
