659b1ba5301546b25cd6c5de4f083fd50ace412d
royeradames <raadamesbonilla@pplweb.com>
2024-06-02 17:29:32 -0400

    header center eye icon

--- app/ui/components/Header.tsx
+++ app/ui/components/Header.tsx
@@ -3,7 +3,6 @@ import { Button } from "@/app/ui/components/Button"
 import Nav from "@/app/ui/components/Nav"
 import { Logo } from "@/app/ui/components/Logo"
 import dynamic from "next/dynamic"
-import Image from "next/image"
 
 const SlIcon = dynamic(
   () => import("@shoelace-style/shoelace/dist/react").then(mod => mod.SlIcon),
@@ -17,17 +16,8 @@ export function Header() {
       <Logo />
       <Nav></Nav>
       <Button variant="secondary">
-        <div className="flex">
-          <Image
-            src="assets/icons/eye.svg"
-            alt="Preview"
-            height={20}
-            width={20}
-            className="sm:hidden"
-            aria-hidden
-          />
-          <span className="sr-only sm:not-sr-only">Preview</span>
-        </div>
+        <SlIcon name="eye" className="h-5 w-5 sm:hidden" />
+        <span className="hidden sm:inline">Preview</span>
       </Button>
     </header>
   )
