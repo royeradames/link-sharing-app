186eb7b8cb94d778517490645dab0043d691a9df
royeradames <raadamesbonilla@pplweb.com>
2024-06-02 11:52:39 -0400

    init login page

---
+++ app/login/page.tsx
@@ -0,0 +1,6 @@
+"use client"
+import React from "react"
+
+export default function Page() {
+  return <h1 className="text-purple">Login</h1>
+}
--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,6 @@ import { PreviewListDemo } from "@/app/demos/PreviewListDemo"
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col gap-8">
-      {/*<DemoGroup />*/}
       <PreviewListDemo />
     </main>
   )
