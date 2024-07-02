c6dc750d77ca1d68619bb3648f2c92575a1f1f4e
royeradames <raadamesbonilla@pplweb.com>
2024-07-02 14:25:53 -0400

    refactor: move field to their own components

--- app/profile-details/page.tsx
+++ app/profile-details/page.tsx
@@ -1,11 +1,11 @@
 "use client"
 import Heading from "@/app/ui/components/Heading"
 import Text from "@/app/ui/components/Text"
-import { InputField } from "@/app/ui/inputs/InputField"
 import { Button } from "@/app/ui/inputs/Button"
 import { useForm } from "react-hook-form"
 import { zodResolver } from "@hookform/resolvers/zod"
 import { z } from "zod"
+import { TextField } from "@/app/profile-details/TextField"
 
 const ProfileDetailsFormSchema = z.object({
   firstName: z.string().min(1, { message: "Can’t be empty" }),
@@ -24,6 +24,31 @@ export default function Page() {
     alert(JSON.stringify(formValues))
   }
 
+  const userDetailsFields: {
+    label: string
+    name: string
+    placeholder: string
+    id: string
+  }[] = [
+    {
+      label: "First name*",
+      name: "firstName",
+      placeholder: "e.g. John",
+      id: "firstName",
+    },
+    {
+      label: "Last name*",
+      name: "lastName",
+      placeholder: "e.g. Appleseed",
+      id: "lastName",
+    },
+    {
+      label: "Email",
+      name: "email",
+      placeholder: "e.g. email@example.com",
+      id: "email",
+    },
+  ]
   return (
     <article aria-label="Profile Details">
       <form
@@ -41,58 +66,14 @@ export default function Page() {
           aria-label="user-details"
           className="flex flex-col justify-center items-center gap-3 self-stretch bg-light-grey p-5 rounded-xl"
         >
-          <div className="flex flex-col justify-center items-start gap-1 self-stretch md:flex-row md:gap-4 md:items-center">
-            <Text
-              as="label"
-              htmlFor="firstName"
-              className=" text-grey text-xs font-normal leading-[150%] md:w-60 md:text-base"
-            >
-              First name*
-            </Text>
-            <InputField
-              iconName=""
-              name="firstName"
-              placeholder="e.g. John"
+          {userDetailsFields.map((fieldProperties, index) => (
+            <TextField
+              key={index}
+              {...fieldProperties}
               register={formMethods.register}
               errors={formMethods.formState.errors}
-              id="firstName"
             />
-          </div>
-
-          <div className="flex flex-col justify-center items-start gap-1 self-stretch md:flex-row md:gap-4 md:items-center">
-            <Text
-              as="label"
-              htmlFor="lastName"
-              className=" text-grey text-xs font-normal leading-[150%] md:w-60 md:text-base"
-            >
-              Last name*
-            </Text>
-            <InputField
-              iconName=""
-              name="lastName"
-              placeholder="e.g. Appleseed"
-              register={formMethods.register}
-              errors={formMethods.formState.errors}
-              id="lastName"
-            />
-          </div>
-          <div className="flex flex-col justify-center items-start gap-1 self-stretch md:flex-row md:gap-4 md:items-center">
-            <Text
-              as="label"
-              htmlFor="email"
-              className=" text-grey text-xs font-normal leading-[150%] md:w-60 md:text-base"
-            >
-              Email
-            </Text>
-            <InputField
-              iconName=""
-              name="email"
-              placeholder="e.g. email@example.com"
-              register={formMethods.register}
-              errors={formMethods.formState.errors}
-              id="email"
-            />
-          </div>
+          ))}
         </section>
 
         <div className="border-t border-borders flex flex-col items-end gap-2 self-stretch px-10 py-6">
