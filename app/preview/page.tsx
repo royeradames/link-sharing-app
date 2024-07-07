22825106eba018b29655937366e27b599be79668
royeradames <raadamesbonilla@pplweb.com>
2024-07-07 12:27:37 -0400

    table nav

--- app/preview/page.tsx
+++ app/preview/page.tsx
@@ -10,8 +10,8 @@ export default function Page() {
       aria-label="Preview Page"
       className="flex flex-col flex-wrap gap-[60px] justify-stretch pb-4"
     >
-      <header className="flex flex-col items-start gap-2 self-stretch bg-white pl-6 pr-4 py-4 rounded-xl ">
-        <nav className="flex items-center gap-4 self-stretch justify-center">
+      <header className="flex flex-col items-start gap-2 self-stretch bg-white pl-6 pr-4 py-4 md:h-[357px] md:bg-purple md:rounded-b-3xl">
+        <nav className="flex items-center gap-4 self-stretch justify-center md:bg-white md:rounded-xl md:justify-between md:pl-6 md:pr-4 md:py-4 ">
           <Link
             href="/profile-details"
             className="flex flex-col justify-center items-center gap-2 flex-[1_0_0] border border-purple px-[27px] py-[11px] rounded-lg border-solid text-purple text-base font-semibold leading-[150%] max-w-max"
