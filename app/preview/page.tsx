f30c6bba7e6325db24f812ef378bf069a935ab43
Royer Adames <royeraadames@gmail.com>
2024-07-07 13:17:53 -0400

    Feature/link 22 desktop preview page (#25)
    * move preview elements to their own component
    
    * mobile view
    
    * table nav
    
    * table view
    
    * sr only heading
    
    * sr heading
    
    ---------
    
    Co-authored-by: royeradames <raadamesbonilla@pplweb.com>

---
+++ app/preview/page.tsx
@@ -0,0 +1,44 @@
+"use client"
+
+import Link from "next/link"
+import { Button } from "@/app/ui/inputs/Button"
+import { PreviewElements } from "@/app/ui/components/PreviewElements"
+
+export default function Page() {
+  return (
+    <article
+      aria-labelledby="preview-page"
+      className="flex flex-col flex-wrap gap-[60px] justify-stretch pb-4 md:bg-light-grey md:items-center"
+    >
+      <h1 className="sr-only" id="preview-page">
+        Preview page
+      </h1>
+      <header className="flex flex-col items-start gap-2 self-stretch bg-white pl-6 pr-4 py-4 md:h-[357px] md:bg-purple md:rounded-b-3xl">
+        <nav
+          className="flex items-center gap-4 self-stretch justify-center md:bg-white md:rounded-xl md:justify-between md:pl-6 md:pr-4 md:py-4 "
+          aria-labelledby="page-option"
+        >
+          <h2 className="sr-only" id="page-option">
+            Page option
+          </h2>
+          <Link
+            href="/profile-details"
+            className="flex flex-col justify-center items-center gap-2 flex-[1_0_0] border border-purple px-[27px] py-[11px] rounded-lg border-solid text-purple text-base font-semibold leading-[150%] max-w-max"
+          >
+            Back to editor
+          </Link>
+          <Button className="w-[155px]">Share Link</Button>
+        </nav>
+      </header>
+      <section
+        aria-labelledby="preview-elements"
+        className="flex-col items-center justify-center gap-14 flex bg-white md:px-14 md:py-12 md:rounded-3xl shadow-[0px_0px_32px_0px_rgba(0,0,0,0.10)] md:w-[349px]  md:items-stretch md:absolute top-44"
+      >
+        <h2 className="sr-only" id="preview-elements">
+          Preview elements
+        </h2>
+        <PreviewElements />
+      </section>
+    </article>
+  )
+}
