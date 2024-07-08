dd4fb8bad30700e4f8b8b2bf605363145ab700e2
royeradames <raadamesbonilla@pplweb.com>
2024-07-08 11:56:16 -0400

    add live preview

--- app/links/page.tsx
+++ app/links/page.tsx
@@ -1,10 +1,17 @@
 import { CustomizeLinks } from "@/app/links/components/CustomizeLinks"
 import { LinksFormProvider } from "@/app/links/components/LinksFormProvider"
+import { LivePreview } from "@/app/ui/components/LivePreview"
 
 export default function Page() {
   return (
     <LinksFormProvider>
-      <CustomizeLinks />
+      <article
+        aria-label="Customize your links"
+        className="flex flex-wrap gap-6 justify-center"
+      >
+        <LivePreview />
+        <CustomizeLinks />
+      </article>
     </LinksFormProvider>
   )
 }
