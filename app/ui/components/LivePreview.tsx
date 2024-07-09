5ebfc57ef1d5bf5ba8561aeeb2f1d8022f9d24db
royeradames <raadamesbonilla@pplweb.com>
2024-07-09 18:15:57 -0400

    make preview scrollable

--- app/ui/components/LivePreview.tsx
+++ app/ui/components/LivePreview.tsx
@@ -4,14 +4,17 @@ import { PreviewElements } from "@/app/ui/components/PreviewElements"
 
 export function LivePreview() {
   return (
-    <div className="hidden xl:flex w-[560px] justify-center items-center gap-2 self-stretch bg-white p-6 rounded-xl">
-      <section
-        aria-label="Live preview"
-        className="bg-[url('/assets/preview-section.svg')] w-[307px] h-[631px] shrink-0 bg-no-repeat flex-col items-center justify-center gap-14 flex "
-      >
-        <h2 className="sr-only">Live preview</h2>
-        <PreviewElements />
-      </section>
-    </div>
+    <>
+      <div className="hidden xl:flex w-[560px] justify-center items-center gap-2 self-stretch bg-white p-6 rounded-xl"></div>
+      <div className="hidden xl:flex w-[560px] justify-center items-center gap-2 self-stretch p-6 rounded-xl fixed left-0">
+        <section
+          aria-label="Live preview"
+          className="bg-[url('/assets/preview-section.svg')] w-[307px] h-[631px] shrink-0 bg-no-repeat flex-col items-center justify-center gap-14 flex "
+        >
+          <h2 className="sr-only">Live preview</h2>
+          <PreviewElements />
+        </section>
+      </div>
+    </>
   )
 }
