e9fd118fcaa9b7bf5a5a54d263d2f77a2d0b29e1
royeradames <raadamesbonilla@pplweb.com>
2024-07-16 22:15:19 -0400

    copy the files from `node_modules/@shoelace-style/shoelace/dist/assets` into your `public/shoelace-assets` folder on every development serve or build

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,24 @@
-// Get the directory path
+import { dirname, resolve } from "path"
+import { fileURLToPath } from "url"
+import CopyPlugin from "copy-webpack-plugin"
 
+const __dirname = dirname(fileURLToPath(import.meta.url));
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-
+  experimental: { esmExternals: 'loose' },
+  webpack: (config, options) => {
+    config.plugins.push(
+      new CopyPlugin({
+        patterns: [
+          {
+            from: resolve(__dirname, 'node_modules/@shoelace-style/shoelace/dist/assets/'),
+            to: resolve(__dirname, 'public/shoelace-assets/assets/')
+          }
+        ]
+      })
+    );
+    return config;
+  }
 }
 
 export default nextConfig
