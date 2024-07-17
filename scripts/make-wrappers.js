3916d5342fa576c53f51a4bdcfb471dc789484df
Royer Adames <royeraadames@gmail.com>
2024-07-16 22:54:32 -0400

    Bug/link 56 shoelace simplify import (#28)
    * set npm install to default to legacy-peer-deps
    
    * wrap shoelace in its own wrapper
    
    * replace dynamic imports for wrapper
    
    * remove shoelace component lag
    
    * lessons learn
    
    * fix prettier error
    
    * lag persist due to ssrSafe having to be true
    
    * delete outdated file
    
    ---------
    
    Co-authored-by: royeradames <raadamesbonilla@pplweb.com>

--- scripts/make-wrappers.js
+++ scripts/make-wrappers.js
@@ -7,11 +7,11 @@ generateReactWrappers(manifest, {
     `@shoelace-style/shoelace/dist/components/${tagName.replace("sl-", "")}/${tagName.replace("sl-", "")}.js`,
   defaultExport: true,
   /**
-   * not true.
-   * Causes lag on reload. You don't event have to use them in 'use client' files just like any other tag.
-   * An important note is that in order to mkae this work, the wrapper generation configuration must have the `ssrSafe` property set to `true`. This will allow the components to register and execute when the client is ready.
+   * Required for build to work
+   * Causes lag on reload
+   * An important note is that in order to make this work, the wrapper generation configuration must have the `ssrSafe` property set to `true`. This will allow the components to register and execute when the client is ready.
    */
-  // ssrSafe: true,
+  ssrSafe: true,
   // todo: fix SlTreeItem auto build issue. For now skip it.
   exclude: ["SlTreeItem"],
 })
